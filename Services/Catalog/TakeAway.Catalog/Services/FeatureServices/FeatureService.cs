81b9ecb9121348f15bb14904a87724ed4209b3ef
Ertuğrul Gökay Duman <ertugrulgokayduman@gmail.com>
2024-07-11 22:34:31 +0300

    Add project files.

---
+++ Services/Catalog/TakeAway.Catalog/Services/FeatureServices/FeatureService.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using MongoDB.Driver;
+using TakeAway.Catalog.Dtos.CategoryDtos;
+using TakeAway.Catalog.Dtos.FeatureDtos;
+using TakeAway.Catalog.Entities;
+using TakeAway.Catalog.Settings;
+
+namespace TakeAway.Catalog.Services.FeatureServices
+{
+    public class FeatureService : IFeatureService
+    {
+        private readonly IMongoCollection<Feature> _featureCollection;
+        private readonly IMapper _mapper;
+
+        public FeatureService(IMapper mapper, IDatabaseSettings _databaseSettings)
+        {
+            var client = new MongoClient(_databaseSettings.ConnectionString);
+            var database = client.GetDatabase(_databaseSettings.DatabaseName);
+            _featureCollection = database.GetCollection<Feature>(_databaseSettings.CategoryCollectionName);
+            _mapper = mapper;
+        }
+
+        public async Task CreateFeatureAsync(CreateFeatureDto createFeatureDto)
+        {
+            var value = _mapper.Map<Feature>(createFeatureDto);
+            await _featureCollection.InsertOneAsync(value);
+        }
+
+        public async Task DeleteFeatureAsync(string id)
+        {
+            await _featureCollection.DeleteOneAsync(x => x.FeatueId == id);
+        }
+
+        public async Task<List<ResultFeatureDto>> GetAllCategories()
+        {
+            var values = await _featureCollection.Find(x => true).ToListAsync();
+            return _mapper.Map<List<ResultFeatureDto>>(values);
+        }
+
+        public async Task<GetByIdFeatureDto> GetByIdFeatureAsync(string id)
+        {
+            var values = await _featureCollection.Find(x => x.FeatueId == id).FirstOrDefaultAsync();
+            return _mapper.Map<GetByIdFeatureDto>(values);
+        }
+
+        public async Task UpdateFeatureAsync(UpdateFeatureDto updateFeatureDto)
+        {
+            var values = _mapper.Map<Feature>(updateFeatureDto);
+            await _featureCollection.FindOneAndReplaceAsync(x => x.FeatueId == updateFeatureDto.FeatueId, values);
+        }
+    }
+}
---
+++ Services/Catalog/TakeAway.Catalog/Settings/IDatabaseSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace TakeAway.Catalog.Settings
+{
+    public interface IDatabaseSettings
+    {
+        public string CategoryCollectionName { get; set; }
+        public string ProductCollectionNeme { get; set; }
+        public string SliderCollectionNeme { get; set; }
+        public string FeatureCollectionNeme { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
