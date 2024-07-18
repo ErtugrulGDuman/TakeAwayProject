2a3436d98152a060aa08d3fba21514b8ce14145f
Ertuğrul Gökay Duman <ertugrulgokayduman@gmail.com>
2024-07-18 22:28:27 +0300

    API layer added.

---
+++ Services/Order/Core/TakeAwayProject.Application/Features/CQRS/Handlers/AddressHandlers/GetAddressByIdQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TakeAwayProject.Application.Features.CQRS.Queries.AddressQueries;
+using TakeAwayProject.Application.Features.CQRS.Results.AddressResults;
+using TakeAwayProject.Application.Interfaces;
+using TakeAwayProject.Domain.Entities;
+
+namespace TakeAwayProject.Application.Features.CQRS.Handlers.AddressHandlers
+{
+    public class GetAddressByIdQueryHandler
+    {
+        private readonly IRepository<Address> _repository;
+
+        public GetAddressByIdQueryHandler(IRepository<Address> repository)
+        {
+            _repository = repository;
+        }
+        public async Task<GetAddressByIdQueryResult> Handle(GetAddressByIdQuery query)
+        {
+            var values = await _repository.GetByIdAsync(query.Id);
+            return new GetAddressByIdQueryResult
+            {
+                AddressId = query.Id,
+                City = values.City,
+                Detail = values.Detail,
+                District = values.District,
+                Name = values.Name,
+                UserId = values.UserId
+            };
+        }
+    }
+}
---
+++ Services/Order/Presentation/TakeAwayProject.OrderApi/Controllers/AddressesController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TakeAwayProject.Application.Features.CQRS.Commands.AddressCommands;
+using TakeAwayProject.Application.Features.CQRS.Handlers.AddressHandlers;
+using TakeAwayProject.Application.Features.CQRS.Queries.AddressQueries;
+
+namespace TakeAwayProject.OrderApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AddressesController : ControllerBase
+    {
+        private readonly GetAddressQueryHandler _getAddressQueryHandler;
+        private readonly GetAddressByIdQueryHandler _getAddressByIdQueryHandler;
+        private readonly CreateAddressCommandHandler _createAddressCommandHandler;
+        private readonly UpdateAddressCommandHandler _updateAddressCommandHandler;
+        private readonly RemoveAddressCommandHandler _removeAddressCommandHandler;
+
+        public AddressesController(GetAddressQueryHandler getAddressQueryHandler, GetAddressByIdQueryHandler getAddressByIdQueryHandler, CreateAddressCommandHandler createAddressCommandHandler, UpdateAddressCommandHandler updateAddressCommandHandler, RemoveAddressCommandHandler removeAddressCommandHandler)
+        {
+            _getAddressQueryHandler = getAddressQueryHandler;
+            _getAddressByIdQueryHandler = getAddressByIdQueryHandler;
+            _createAddressCommandHandler = createAddressCommandHandler;
+            _updateAddressCommandHandler = updateAddressCommandHandler;
+            _removeAddressCommandHandler = removeAddressCommandHandler;
+        }
+        [HttpGet]
+        public async Task<IActionResult> AddressList()
+        {
+            var value = await _getAddressQueryHandler.Handle();
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAddress(CreateAddressCommand command)
+        {
+            await _createAddressCommandHandler.Handle(command);
+            return Ok("Ekleme işlemi yapıldı");
+        }
+        [HttpDelete]
+        public async Task<IActionResult> DeleteAddress(int id)
+        {
+            await _removeAddressCommandHandler.Handle(new RemoveAddressCommand(id));
+            return Ok("Silme işlemi yapıldı");
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAddress(int id)
+        {
+            var values = await _getAddressByIdQueryHandler.Handle(new GetAddressByIdQuery(id));
+            return Ok(values);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateAddress(UpdateAddressCommand command)
+        {
+            await _updateAddressCommandHandler.Handle(command);
+            return Ok("Güncelleme yapıldı");
+        }
+    }
+}
